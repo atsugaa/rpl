a8d3ef5473aaaa922501b9dc4f04cb8ccf0c971f
ali-vian <alivian7373@gmail.com>
2024-06-02 20:32:14 +0700

    lesles

--- app/base.php
+++ app/base.php
@@ -1,6 +1,6 @@
 <?php
-	define("BASEURL", "http://localhost/rpl-main/rpl");
-	define("BASEPATH", $_SERVER["DOCUMENT_ROOT"]."/rpl-main/rpl");
+	define("BASEURL", "http://localhost:8080/rpl");
+	define("BASEPATH", $_SERVER["DOCUMENT_ROOT"]."/rpl");
 	function connect($host, $db, $user, $password) {
 			$dsn = "mysql:host=$host;dbname=$db;charset=UTF8";
 		try {
@@ -10,5 +10,5 @@
 			die($e->getMessage());
 		}
 	}
-	return define("DB", connect('localhost', 'rpl', 'root', ''));
+	return define("DB", connect('localhost', 'rpl2', 'root', ''));
 ?>
